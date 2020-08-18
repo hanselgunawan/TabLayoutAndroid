19208d0a64ac4ea5da25799a22d2e6e4f67cd3db
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-17 18:43:42 -0700

    added SmartFragmentStatePagerAdapter

---
+++ app/src/main/java/com/hanseltritama/tablayoutdemo/SmartFragmentStatePagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.hanseltritama.tablayoutdemo
+
+import android.util.SparseArray
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+abstract class SmartFragmentStatePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    private val registeredFragments = SparseArray<Fragment>()
+
+    // Register the fragment when the item is instantiated
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val fragment = super.instantiateItem(container, position) as Fragment
+        registeredFragments.put(position, fragment)
+        return fragment
+    }
+
+    // Unregister when the item is inactive
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        registeredFragments.remove(position)
+        super.destroyItem(container, position, `object`)
+    }
+
+    fun getRegisteredFragment(position: Int) : Fragment {
+        return registeredFragments[position]
+    }
+
+}
